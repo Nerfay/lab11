ec4c696e1af9d97f85816f103d3e7bc2c8a0c96f
Nerfay <101548076+Nerfay@users.noreply.github.com>
2023-11-29 23:46:30 +0200

    Add files via upload

---
+++ lab11/lab10/Person.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace lab10;
+
+public class Person
+{
+    private int _id;
+    private string _name;
+    private int _age;
+    private int _gender;
+
+    public int Id
+    {
+        get => _id;
+        set => _id = value;
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public int Age
+    {
+        get => _age;
+        set => _age = value;
+    }
+
+    public int Gender
+    {
+        get => _gender;
+        set => _gender = value;
+    }
+}
---
+++ lab11/lab10/PersonComparer.cs
@@ -0,0 +1,29 @@
+﻿namespace lab10;
+
+public class PersonComparer : IComparer<Person>
+{
+    private string _propertyToSort;
+
+    public PersonComparer(string propertyToSort)
+    {
+        _propertyToSort = propertyToSort;
+    }
+
+    public int Compare(Person x, Person y)
+    {
+        // Compare by the specified property
+        switch (_propertyToSort.ToLower())
+        {
+            case "id":
+                return x.Id.CompareTo(y.Id);
+            case "name":
+                return string.Compare(x.Name, y.Name, StringComparison.Ordinal);
+            case "age":
+                return x.Age.CompareTo(y.Age);
+            case "gender":
+                return x.Gender.CompareTo(y.Gender);
+            default:
+                throw new ArgumentException("Invalid property name for sorting");
+        }
+    }
+}
